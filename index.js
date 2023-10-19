6b7d95238aa5a8235b0f05be3d3451e47244d171
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-19 14:21:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const createApp = () => {
   app.use('/api', require('./api'))
 
   // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')))
+  app.use(express.static(path.join(__dirname, + './public/index.html')))
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
